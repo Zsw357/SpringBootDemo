b778cb547a4aedd2ad8d6b6dc59f99d1214ca0ae
Zsw <1426918047@QQ.COM>
2023-03-28 09:35:46 +0800

    提交

--- src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
@@ -3,9 +3,8 @@ package com.example.springbootdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.springbootdemo")
 public class SpringBootDemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
--- src/main/java/com/example/springbootdemo/controller/HelloController.java
+++ src/main/java/com/example/springbootdemo/controller/HelloController.java
@@ -1,5 +1,18 @@
-package com.example.springbootdemo.controller;/**
-@author Zsw
-@create 2023-02-20 14:57
-*/public class HelloController {
+package com.example.springbootdemo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Zsw
+ * @create 2023-02-20 14:57
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("hello1")
+    public String hello1(){
+        return "Zsw";
+    }
 }
